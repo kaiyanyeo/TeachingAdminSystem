96b53c15ddd54037b77160014cfa4d24f119a479
kaiyan <kaiyan.yeo@gmail.com>
2019-02-18 23:39:31 +0800

    Modify registers to be multiple queries

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,9 @@ var router = express.Router();
  * A teacher registers one or more students to a specified teacher.
  */
 router.post('/register', function(req, res, next) {
-	var insertTeacherSql = 'INSERT IGNORE INTO Teachers (temail) VALUES (?)';
-	var insertStudentSql = 'INSERT IGNORE INTO Students (semail) VALUES ?';
-	var insertRegisterSql = 'INSERT IGNORE INTO Registers (temail, semail) VALUES ?';
+	var registerSql = 'INSERT IGNORE INTO Teachers (temail) VALUES (?); ' +
+			'INSERT IGNORE INTO Students (semail) VALUES ?; ' +
+			'INSERT IGNORE INTO Registers (temail, semail) VALUES ?';
 	var registerPairs = [];
 
 	var semail = req.body.students;
@@ -18,15 +18,7 @@ router.post('/register', function(req, res, next) {
 		registerPairs.push([temail, semail[i]]);
 	}
 
-	connection.query(insertTeacherSql, temail, function(error, results, fields) {
-		if(error)
-			res.send(JSON.stringify({"status": 500, "error": error}));
-	}),
-	connection.query(insertStudentSql, [students], function(error, results, fields) {
-		if(error)
-			res.send(JSON.stringify({"status": 500, "error": error}));
-	}),
-	connection.query(insertRegisterSql, [registerPairs], function(error, results, fields) {
+	connection.query(registerSql, [temail, students, registerPairs], function(error, results, fields) {
 		if(error) {
 			res.send(JSON.stringify({"status": 500, "error": error}));
 		} else {
